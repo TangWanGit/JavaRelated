82bb58181fd0d443c2614d463d81b42a497c2653
TangWanGit <13967197743@163.com>
2020-05-26 18:43:17 +0800

    是否为回文字符串

--- Algorithm/src/main/java/com/tangwan/algorithm/c06_list_interview/T02_IsPalindromeList.java
+++ Algorithm/src/main/java/com/tangwan/algorithm/c06_list_interview/T02_IsPalindromeList.java
@@ -17,9 +17,62 @@ import java.util.Stack;
  */
 public class T02_IsPalindromeList {
     public static void main(String[] args) {
+        Node head = null;
+        printLinkedList(head);
+        System.out.print(isPalindrome1(head) + " | ");
+        System.out.print(isPalindrome2(head) + " | ");
+        System.out.println(isPalindrome3(head) + " | ");
+        System.out.println("==========================");
 
+        head = new Node(1);
+        test(head);
+
+        head = new Node(1);
+        head.next = new Node(2);
+        test(head);
+
+        head = new Node(1);
+        head.next = new Node(1);
+        test(head);
+
+        head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        test(head);
+
+        head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(1);
+        test(head);
+
+        head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(1);
+        test(head);
+
+        head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(2);
+        head.next.next.next = new Node(1);
+        test(head);
+
+        head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(2);
+        head.next.next.next.next = new Node(1);
+        test(head);
     }
 
+    private static void test(Node head) {
+        printLinkedList(head);
+        System.out.print(isPalindrome1(head) + " | ");
+        System.out.print(isPalindrome2(head) + " | ");
+        System.out.println(isPalindrome3(head) + " | ");
+        printLinkedList(head);
+        System.out.println("==========================");
+    }
     // need n extra space
 
     /**
@@ -57,7 +110,7 @@ public class T02_IsPalindromeList {
      * @return
      */
     public static boolean isPalindrome2(Node head) {
-        if (head == null || head.next == null || head.next.next == null) {
+        if (head == null || head.next == null) {
             return true;
         }
 
@@ -85,11 +138,65 @@ public class T02_IsPalindromeList {
 
     /**
      * need O(1) extra space
+     *
      * @param head
+     *
      * @return
      */
     public static boolean isPalindrome3(Node head) {
-        return true;
+        if (head == null || head.next == null) {
+            return true;
+        }
+        Node n1 = head;
+        Node n2 = head;
+        while (n2.next != null && n2.next.next != null) {
+            n1 = n1.next;
+            n2 = n2.next.next;
+        }
+        // n1 mid
+
+        n2 = n1.next;
+        n1.next = null;
+
+        Node n3 = null;
+        while (n2 != null) {
+            n3 = n2.next;
+            n2.next = n1;
+            n1 = n2;
+            n2 = n3;
+        }
+
+        n3 = n1;
+        n2 = head;
+        boolean res = true;
+        while (n1 != null && n2 != null) {
+            if (n1.value != n2.value) {
+                res = false;
+                break;
+            }
+            n1 = n1.next;
+            n2 = n2.next;
+        }
+
+        n1 = n3.next;
+        n3.next = null;
+        while (n1 != null) {
+
+            n2 = n1.next;
+            n1.next = n3;
+            n3 = n1;
+            n1 = n2;
+        }
+        return res;
+    }
+
+    public static void printLinkedList(Node node) {
+        System.out.println("Linked list: ");
+        while (node != null) {
+            System.out.print(node.value + " ");
+            node = node.next;
+        }
+        System.out.println();
     }
 
     public static class Node {
