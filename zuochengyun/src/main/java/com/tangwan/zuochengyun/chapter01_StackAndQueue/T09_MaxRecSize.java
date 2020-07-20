a425b3fec4c14d479d1f6355a157f8fa684139ad
TangWanGit <13967197743@163.com>
2020-07-20 17:04:34 +0800

    左程云-算法  求最大子矩阵大小

---
+++ zuochengyun/src/main/java/com/tangwan/zuochengyun/chapter01_StackAndQueue/T09_MaxRecSize.java
@@ -0,0 +1,67 @@
+/*
+ * File Name:T09_MaxRecSize is created on 2020-07-20 16:17 by Zhao Xiaoli
+ *
+ * Copyright (c) 2020, xiaoyujiaoyu technology All Rights Reserved.
+ *
+ */
+package com.tangwan.zuochengyun.chapter01_StackAndQueue;
+
+import java.util.Stack;
+
+/**
+ * @author Zhao Xiaoli
+ * @Description : T09_MaxRecSize
+ * 求最大子矩阵大小
+ * @date 2020-07-20 16:17
+ * @since JDK 1.8
+ */
+public class T09_MaxRecSize {
+    public static void main(String[] args) {
+        int[][] arr = {{1}, {1}, {1}, {0}};
+        System.out.println(maxRecSize(arr));
+
+        int[][] arr2 = {{1, 0, 1, 1}, {1, 1, 1, 1}, {1, 1, 1, 0}};
+        System.out.println(maxRecSize(arr2));
+    }
+
+    public static int maxRecSize(int[][] map) {
+        if (map == null || map.length == 0 || map[0].length == 0) {
+            return 0;
+        }
+
+        int maxArea = 0;
+        int[] height = new int[map[0].length];
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[0].length; j++) {
+                height[j] = map[i][j] == 0 ? 0 : height[j] + 1;
+            }
+            maxArea = Math.max(maxArea, maxRecFormBottom(height));
+        }
+        return maxArea;
+    }
+
+    private static int maxRecFormBottom(int[] height) {
+        if (height == null || height.length == 0) {
+            return 0;
+        }
+        int maxArea = 0;
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < height.length; i++) {
+            while ((!stack.isEmpty()) && height[i] <= height[stack.peek()]) {
+                int j = stack.pop();
+                int k = stack.isEmpty() ? -1 : stack.peek();
+                int curArea = (i - k - 1) * height[j];
+                maxArea = Math.max(maxArea, curArea);
+            }
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()) {
+            int j = stack.pop();
+            int k = stack.isEmpty() ? -1 : stack.peek();
+            int curArea = (height.length - k - 1) * height[j];
+            maxArea = Math.max(maxArea, curArea);
+        }
+        return maxArea;
+    }
+}
